61248ccb4fded38e2d1a39107ca74b3aaa60d118
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2024-06-19 17:00:22 +0100

    remove axios and oauth

--- app/components/nutrition/Nutrition.jsx
+++ app/components/nutrition/Nutrition.jsx
@@ -1,14 +1,40 @@
+"use client";
 import React from "react";
+import { useState } from "react";
 import Nutri from "./Nutrition.module.css";
 import LargeDivider from "../labelDividers/LargeDivider";
 import MediumDivider from "../labelDividers/MediumDivider";
 import ThinDivider from "../labelDividers/ThinDivider";
 
 const Nutrition = () => {
+  const [inputText, setInputText] = useState();
+  const [apiResult, setApiResult] = useState();
+
+  async function buttonPress(e) {
+    e.preventDefault();
+    // console.log(inputText);
+    const id = "2fd99e61";
+    const key = "96e61a7ec4e69fccadeb8daf3c7ef874";
+    const temporary = await fetch(
+      `https://api.edamam.com/api/food-database/v2/parser?app_id=${id}&app_key=${key}&ingr=${inputText}&nutrition-type=cooking`
+    );
+    const result = await temporary.json();
+    // console.log(result);
+    setApiResult(result);
+  }
+  //   console.log(apiResult);
+  //   console.log(apiResult.parsed[0].food.nutrients);
+
+  let nutrients = apiResult ? apiResult.parsed[0].food.nutrients : "null";
+
   return (
     <section className={Nutri.section}>
-      <form>
-        <input className={Nutri.input} placeholder="Feed me..."></input>
+      <form onSubmit={buttonPress}>
+        <input
+          className={Nutri.input}
+          placeholder="Feed me..."
+          onChange={(e) => setInputText(e.target.value)}
+        ></input>
         <button className={Nutri.searchButton}>Search</button>
       </form>
 
@@ -16,18 +42,20 @@ const Nutrition = () => {
         <h1>Nutrition Facts</h1>
         <div className={Nutri.servingSize}>
           <p>Serving size</p>
-          <p>1 slice</p>
+          <p>100gr</p>
         </div>
         <LargeDivider />
         <div className={Nutri.caloriesLine}>
           <p>Calories</p>
-          <p className={Nutri.calories}>240</p>
+          <p className={Nutri.calories}>
+            {nutrients ? nutrients.ENERC_KCAL : "--"}
+          </p>
         </div>
         {/* <div className={Nutri.dividerSmall}></div> */}
         <MediumDivider />
         <div className={Nutri.macrosMajor}>
           <p>Total fat</p>
-          <p>4g</p>
+          <p>{nutrients ? nutrients.FAT : "--"}</p>
         </div>
         <ThinDivider />
         <div className={Nutri.macrosMinor}>
@@ -42,7 +70,7 @@ const Nutrition = () => {
         <ThinDivider />
         <div className={Nutri.macrosMajor}>
           <p>Total Carbohydrates</p>
-          <p>46g</p>
+          <p>{nutrients ? nutrients.CHOCDF : "--"}</p>
         </div>
         <ThinDivider />
         <div className={Nutri.macrosMinor}>
@@ -57,7 +85,7 @@ const Nutrition = () => {
         <ThinDivider />
         <div className={Nutri.macrosMajor}>
           <p>Protein</p>
-          <p>11g</p>
+          <p>{nutrients ? nutrients.PROCNT : "--"}</p>
         </div>
         <LargeDivider />
         <div className={Nutri.micros}>
